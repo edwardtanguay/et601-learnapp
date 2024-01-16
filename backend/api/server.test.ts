630bb2cf2ca6602ec0b29a6ef40bf6d52ff87d98
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-16 12:56:00 +0100

    finished

--- backend/api/server.test.ts
+++ backend/api/server.test.ts
@@ -2,7 +2,7 @@ import { expect, describe, it } from 'vitest';
 import request from 'supertest';
 import { app } from './server';
 
-describe('/GET', async () => {
+describe('GET /', async () => {
 	const response = await request(app).get('/');
 	const responseBody = JSON.parse(response.text);
 	it('receives correct HTTP code', () => {
@@ -15,8 +15,26 @@ describe('/GET', async () => {
 		expect(responseBody).toHaveProperty('appName');
 	});
 	it('has an appName that is not empty', () => {
-		expect(responseBody.appName).not.toEqual(''); 
+		expect(responseBody.appName).not.toEqual('');
 	});
+});
 
+describe('GET /api/flashcards/info/logs', async () => {
+	const response = await request(app).get('/api/flashcards/info/logs');
+	it('receives correct HTTP code', () => {
+		expect(response.statusCode).toBe(200);
+	});
+	it('has correct content-type', () => {
+		expect(response.headers['content-type']).toMatch(/text\/html/);
+	});
+});
 
+describe('GET /api/flashcards', async () => {
+	const response = await request(app).get('/api/flashcards');
+	it('receives correct HTTP code', () => {
+		expect(response.statusCode).toBe(200);
+	});
+	it('has correct content-type', () => { 
+		expect(response.headers['content-type']).toMatch(/application\/json/);
+	});
 }); 
