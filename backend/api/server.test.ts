2ee8fe6ef2ef3432a15595bcdaa16815f2047d7e
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-16 01:11:17 +0100

    root test finished

--- backend/api/server.test.ts
+++ backend/api/server.test.ts
@@ -1,10 +1,22 @@
-import { test, expect, describe, it } from 'vitest';
+import { expect, describe, it } from 'vitest';
 import request from 'supertest';
 import { app } from './server';
 
-describe('root route works', () => {
-	test('GET /', async () => {
-		const response = await request(app).get('/');
+describe('/GET', async () => {
+	const response = await request(app).get('/');
+	const responseBody = JSON.parse(response.text);
+	it('receives correct HTTP code', () => {
 		expect(response.statusCode).toBe(200);
 	});
-}) 
+	it('has correct content-type', () => {
+		expect(response.headers['content-type']).toMatch(/application\/json/);
+	});
+	it('JSON object contains correct properties', () => {
+		expect(responseBody).toHaveProperty('appName');
+	});
+	it('has an appName that is not empty', () => {
+		expect(responseBody.appName).not.toEqual(''); 
+	});
+
+
+}); 
